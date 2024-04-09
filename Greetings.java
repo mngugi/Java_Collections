50d4cbe717db4309ecb749ea58890c7745957dca
M.ngugi <mngugi7@gmail.com>
2024-04-09 04:00:45 +0300

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,6 @@
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Learning the job !!");
+        System.out.println("Assignment 1 Complete !!!!");
+    }
+}
