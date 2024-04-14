7c8e08d64769ad9fd52e3e7afd3c73afd7bbb0a7
M.ngugi <mngugi7@gmail.com>
2024-04-14 20:46:53 +0300

    mavenproject1/pom.xml

---
+++ mavenproject1/src/main/java/Sort.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author x1869
+ */
+public class Sort {
+    public static void main(String[] args) {
+    System.out.println("Hello Java!");
+    
+    System.out.println("You were born in 1995");
+
+    /**Sun Microsystems announced the release of Java in 1995 */
+    
+    System.out.println("You were created by James Gosling");
+    
+		/**James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2! */
+    
+    System.out.println("You are a fun language!");
+  }
+    
+}
