7adf9c30c47946553aa98c11df8029a93956bb75
mngugi <mngugi7@gmail.com>
2024-10-07 00:24:30 +0300

    .idea/.gitignore

---
+++ javTest.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class testIfJava {
+
+	public static void main(String[] args) {
+		System.out.println("Java is installed and Working, run the array :) !!");
+		int[] numbers = new int[6];
+		numbers[0] = 120 ;
+		numbers[1] = 210;
+		numbers[2] = 220;
+		numbers[3] = 963;
+		numbers[4] = 632;
+		numbers[5] = 542;
+		System.out.println(Arrays.toString(numbers));
+	}
+}
