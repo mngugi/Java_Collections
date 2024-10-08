3d0b1fbf722d011adf6de6affea27029785dc859
mngugi <mngugi7@gmail.com>
2024-10-08 12:27:28 +0300

    Create speedLimit.java

---
+++ Java2024/src/speed/speedLimit.java
@@ -0,0 +1,10 @@
+package speed;
+
+public class speedLimit {
+	public static void main(String[] args) {
+		
+		int speedLimits = 80;
+		System.out.printf("The Speed Limit is:",speedLimits);
+	}
+
+}
