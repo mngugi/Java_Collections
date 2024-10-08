f6a9c412db0dbf237d07c0049c383382da4eb0a6
mngugi <mngugi7@gmail.com>
2024-10-08 23:17:00 +0300

    Update speedLimit.java

--- Java2024/src/speed/speedLimit.java
+++ Java2024/src/speed/speedLimit.java
@@ -4,6 +4,6 @@ public class speedLimit {
     public static void main(String[] args) {
         
         int speedLimit = 80;
-        System.out.println(speedLimit);
+        System.out.println("The speed limit for a school zone is:"+speedLimit +"KPH.");
     }
 }
