c1017372b6ab3cc589b15edea86cee55ea5feb05
mngugi <mngugi7@gmail.com>
2024-10-08 12:30:31 +0300

    Update speedLimit.java

--- Java2024/src/speed/speedLimit.java
+++ Java2024/src/speed/speedLimit.java
@@ -4,7 +4,7 @@ public class speedLimit {
 	public static void main(String[] args) {
 		
 		int speedLimits = 80;
-		System.out.printf("The Speed Limit is:",speedLimits);
+		System.out.println(speedLimits);
 	}
 
 }
