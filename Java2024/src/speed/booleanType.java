ce7ede8dbd0d95fc51a9944c3a6e1897e3ede038
mngugi <mngugi7@gmail.com>
2024-10-08 23:23:26 +0300

    Create booleanType.java

---
+++ Java2024/src/speed/booleanType.java
@@ -0,0 +1,14 @@
+package speed;
+
+public class booleanType {
+
+	public static void main(String[] args) {
+		//boolean is either True or False
+		
+		boolean flag = true;
+		System.out.println("The boolean type is " + flag +".");
+		
+
+	}
+
+}
