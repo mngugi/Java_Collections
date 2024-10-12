9f6c20b2d7cc3dcbffe288d665ab6d005d2d6abe
mngugi <mngugi7@gmail.com>
2024-10-12 15:30:17 +0300

    Update morgageInterest.java

--- Java2024/src/speed/morgageInterest.java
+++ Java2024/src/speed/morgageInterest.java
@@ -1,5 +1,11 @@
 package speed;
 
 public class morgageInterest {
+	// declare required variables
+	String NAME = "Tom Dick Harry";
+	double BALANCE = 520000.00;
+	double RATE = 0.12;
+	double INTEREST;
+	
 
 }
