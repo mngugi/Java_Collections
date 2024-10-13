9630448c338db85ad25ee8d7221648c1d0020e79
mngugi <mngugi7@gmail.com>
2024-10-13 10:46:31 +0300

    Create AritmetricOperators.java

---
+++ Java2024/src/speed/AritmetricOperators.java
@@ -0,0 +1,12 @@
+package speed;
+
+public class AritmetricOperators {
+	public static void main(String args) {
+		// declare variables
+		int i = 56, j= 20;
+		
+		//addition 
+		System.out.print("i+j" + i+j);
+	}
+
+}
