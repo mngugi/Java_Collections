954888ef6d63c5faa7ae2126ae168291fe2b3c11
mngugi <mngugi7@gmail.com>
2024-10-13 11:06:07 +0300

    Update AritmetricOperators.java

--- Java2024/src/speed/AritmetricOperators.java
+++ Java2024/src/speed/AritmetricOperators.java
@@ -6,15 +6,15 @@ public class AritmetricOperators {
 		int i = 56, j= 20;
 		
 		//Addition 
-		System.out.println("i+j  " + (i+j));
+		System.out.println("i+j=  " + (i+j));
 		// Subtraction
-		System.out.println("i-j  " + (i-j));
+		System.out.println("i-j=  " + (i-j));
 		// Multiplication
-		System.out.println("i*j  " + (i*j));
+		System.out.println("i*j=  " + (i*j));
 		// Division 
-		System.out.println("i/j  " + (i/j));
+		System.out.println("i/j=  " + (i/j));
 		// Modulo
-		System.out.println("i%j  " + (i%j));
+		System.out.println("i%j=  " + (i%j));
 		
 	}
 
