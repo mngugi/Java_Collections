c1bb3447b02a2aeeee21e08458674fc4b3118304
mngugi <mngugi7@gmail.com>
2024-10-13 11:07:05 +0300

    Update AritmetricOperators.java

--- Java2024/src/speed/AritmetricOperators.java
+++ Java2024/src/speed/AritmetricOperators.java
@@ -3,7 +3,7 @@ package speed;
 public class AritmetricOperators {
 	public static void main(String[] args) {
 		// declare variables
-		int i = 56, j= 20;
+		int i = 56, j = 20;
 		
 		//Addition 
 		System.out.println("i+j=  " + (i+j));
