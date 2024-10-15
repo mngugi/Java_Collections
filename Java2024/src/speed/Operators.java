81e26f0ed63ea4dde7b8937e03991efeb7a4315b
mngugi <mngugi7@gmail.com>
2024-10-15 16:52:14 +0300

    Update Operators.java

--- Java2024/src/speed/Operators.java
+++ Java2024/src/speed/Operators.java
@@ -3,7 +3,9 @@ package speed;
 public class Operators {
 	public static void main(String[] args) {
 		int i = 152, j = 874;
-		System.out.println("i is " + i + " and j is" +j);
+		System.out.println("i is " + i + " and j is " +j);
+		System.out.println(i==j);
+		
 		
 
 }
