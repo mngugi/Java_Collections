bd89748a2168cd610f3d87a60dcbe32001c455cd
mngugi <mngugi7@gmail.com>
2024-10-15 16:57:23 +0300

    Update Operators.java

--- Java2024/src/speed/Operators.java
+++ Java2024/src/speed/Operators.java
@@ -4,7 +4,34 @@ public class Operators {
 	public static void main(String[] args) {
 		int i = 152, j = 874;
 		System.out.println("i is " + i + " and j is " +j);
-		System.out.println(i==j);
+		System.out.println(i==j); // expected false
+		// != operator
+	    System.out.println(i != j);  // true
+
+	    // > operator
+	    System.out.println(i > j);  // false
+
+	    // < operator
+	    System.out.println(i < j);  // true
+
+	    // >= operator
+	    System.out.println(i >= j);  // false
+
+	    // <= operator
+	    
+	    System.out.print("Logical Operators.");
+	    // && operator
+	    System.out.println((5 > 3) && (8 > 5));  // true
+	    System.out.println((5 > 3) && (8 < 5));  // false
+
+	    // || operator
+	    System.out.println((5 < 3) || (8 > 5));  // true
+	    System.out.println((5 > 3) || (8 < 5));  // true
+	    System.out.println((5 < 3) || (8 < 5));  // false
+
+	    // ! operator
+	    System.out.println(!(5 == 3));  // true
+	    System.out.println(!(5 > 3));  // false
 		
 		
 
