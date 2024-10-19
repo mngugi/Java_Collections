ee39b46a0c09cafe19635bf8ec6eb2f19a3df151
mngugi <mngugi7@gmail.com>
2024-10-19 19:04:17 +0300

    DataStructureandAlgorithms/src/dataStructure/bigOnotatio.class

--- DataStructureandAlgorithms/src/dataStructure/bigOnotatio.java
+++ DataStructureandAlgorithms/src/dataStructure/bigOnotatio.java
@@ -2,8 +2,9 @@
 package dataStructure;
 
 public class bigOnotatio {
-	public static void main(int[] numbers) {
-		// runtime is O(1) as the program is printing one integer,so
+	public static void main(String[] args) {
+		int[] numbers = {3,1,2,3,0};
+		// runtime is O(1) as the program is printing one integer
 		// which is O(n)
 		System.out.println(numbers[0]);
 		
