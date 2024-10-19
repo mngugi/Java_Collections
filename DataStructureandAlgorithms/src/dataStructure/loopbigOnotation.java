e951f320c6607b6e38d2177a9975bb54ac529780
mngugi <mngugi7@gmail.com>
2024-10-19 19:37:28 +0300

    Create loopbigOnotation.java

---
+++ DataStructureandAlgorithms/src/dataStructure/loopbigOnotation.java
@@ -0,0 +1,10 @@
+package dataStructure;
+
+public class loopbigOnotation {
+	public static void main() {
+		int[] numbers = {3,1,2,3,0};
+		for (int i = 0; i < numbers.length; i++)
+			System.out.println(numbers[i]);
+	}
+
+}
