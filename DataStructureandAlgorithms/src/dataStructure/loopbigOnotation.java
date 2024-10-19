022d22f58dc7760f21cd87a210ea8dd153a8a106
mngugi <mngugi7@gmail.com>
2024-10-19 19:45:27 +0300

    loopbigOnotation.class

--- DataStructureandAlgorithms/src/dataStructure/loopbigOnotation.java
+++ DataStructureandAlgorithms/src/dataStructure/loopbigOnotation.java
@@ -1,7 +1,7 @@
 package dataStructure;
 
 public class loopbigOnotation {
-	public static void main(int[] args) {
+	public static void main(String[] args) {
 		int[] numbers = {3,1,2,3,0};
 		for (int i = 0; i < numbers.length; i++)
 			System.out.println(numbers[i]);
