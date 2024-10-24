fae5dc7488c417469c95a7935d9de3875ab8ae08
mngugi <mngugi7@gmail.com>
2024-10-24 18:50:31 +0300

    Update searchName.java

--- Java2024/src/speed/searchName.java
+++ Java2024/src/speed/searchName.java
@@ -2,12 +2,30 @@ package speed;
 
 public class searchName {
 	public static void main(String[] args) {
-		String[] names = new String[2];
+		String[] names = new String[4];
 		
 		names[0] = "katuma katavi";
 		names[1] = "kanini kadogo";
 		names[2] = "mutego kushika";
 		names[3] = "kanyundo kupiga";
-	}
-
+		
+		 // Search for the name in the array
+        String targetName = "katuma katavi";
+        boolean found = false;
+        
+        // Loop through the array to find the name
+        for (String name : names) {
+            if (name.equals(targetName)) {
+                found = true;
+                break;
+            }
+        }
+        
+        // Print result
+        if (found) {
+            System.out.println("Name found");
+        } else {
+            System.out.println("Name not found");
+        }
+    }
 }
