afb75b3cd5f3f94d4f2d3aa4d799a9ca66918115
mngugi <mngugi7@gmail.com>
2024-10-24 17:40:12 +0300

    Update ifStatement.java

--- Java2024/src/speed/ifStatement.java
+++ Java2024/src/speed/ifStatement.java
@@ -2,7 +2,17 @@ package speed;
 
 public class ifStatement {
 	public static void main(String[] args) {
+		int j = 56;
+		int i = 100;
+		// if number is j is greater than i
+		
+		if (j >= i) {
+			System.out.println("J is greater than i value");
 		
 	}
+		
+			
+	}	
+
 
 }
