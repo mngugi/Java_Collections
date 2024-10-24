2cbfe9d385f7e6be0cb688a5bd8913c2f4350b77
mngugi <mngugi7@gmail.com>
2024-10-24 19:33:48 +0300

    Update searchName.java

--- Java2024/src/speed/searchName.java
+++ Java2024/src/speed/searchName.java
@@ -3,7 +3,7 @@ package speed;
 public class searchName {
 	public static void main(String[] args) {
 		String[] names = new String[4];
-		
+		// array initialization 
 		names[0] = "katuma katavi";
 		names[1] = "kanini kadogo";
 		names[2] = "mutego kushika";
