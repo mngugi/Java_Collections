316064f5e34454d33c68aae75073cc88fcfb1d03
mngugi <mngugi7@gmail.com>
2024-10-24 18:17:07 +0300

    DataStructureandAlgorithms/src/dataStructure/defineArray.java

---
+++ Java2024/src/speed/searchName.java
@@ -0,0 +1,13 @@
+package speed;
+
+public class searchName {
+	public static void main(String[] args) {
+		String[] names = new String[2];
+		
+		names[0] = "katuma katavi";
+		names[1] = "kanini kadogo";
+		names[2] = "mutego kushika";
+		names[3] = "kanyundo kupiga";
+	}
+
+}
