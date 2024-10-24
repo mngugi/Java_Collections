98bf5b1a5a52d804473ab96082ae30268f082bc7
mngugi <mngugi7@gmail.com>
2024-10-24 10:18:46 +0300

    Update defineArray.java

--- DataStructureandAlgorithms/src/dataStructure/defineArray.java
+++ DataStructureandAlgorithms/src/dataStructure/defineArray.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 public class defineArray {
 	public static void main(String[] args) {
 		String [] names = new String[4];
+		names[0] = "katuma katavi";
+		names[1] = "kanini kadogo";
+		names[2] = "mutego kushika";
+		names[3] = "kanyundo kupiga";
 		System.out.println(Arrays.toString(names));
 	}
 
