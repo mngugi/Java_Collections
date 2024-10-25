b7bb31865797c62616205339bc85bd2f48670c54
mngugi <mngugi7@gmail.com>
2024-10-25 12:23:22 +0300

    Update findNumber.java

--- DataStructureandAlgorithms/src/dataStructure/findNumber.java
+++ DataStructureandAlgorithms/src/dataStructure/findNumber.java
@@ -1,9 +1,22 @@
 package dataStructure;
+import arrays.*;
 
 public class findNumber {
 	public static void main(int[] args) {
 		
 		int[] numbers = {3,1,2,3,0};
+		
+		int targetNumber = 3;
+		boolean found = true;
+		
+		for (int number:numbers) {
+			if(numbers.equals(targetNumber)) {
+				found = true;
+				break;
+				
+			}
+		}
+		
 		
 	}
 
