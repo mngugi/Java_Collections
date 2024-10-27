3ae2fa3aaf4a5833aff8ba7ac710a4dedf59cb3b
mngugi <mngugi7@gmail.com>
2024-10-27 23:14:08 +0300

    Update ifjava.java

--- DataStructureandAlgorithms/src/dataStructure/ifjava.java
+++ DataStructureandAlgorithms/src/dataStructure/ifjava.java
@@ -3,12 +3,18 @@ package dataStructure;
 public class ifjava {
 	public static void main(String[] args) {
 		int integer = 1;
+		String top = "first";
 		// check if the number is less than 0
 		if(integer < 0) {
 			System.out.println("The number is negative.");
 		}
 
 		    System.out.print("Statement outside the block.");
-	}
+	
+		if(top == "first") {
+			System.out.print("Top is the first one in the stack");
+			
+		}
 
+ }
 }
