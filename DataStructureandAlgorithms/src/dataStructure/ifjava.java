75974a65875ce2527fdb8544e03f52294e2a57c7
mngugi <mngugi7@gmail.com>
2024-10-27 22:56:53 +0300

    Create ifjava.java

---
+++ DataStructureandAlgorithms/src/dataStructure/ifjava.java
@@ -0,0 +1,14 @@
+package dataStructure;
+
+public class ifjava {
+	public static void main(String[] args) {
+		int integer = 65;
+		// check if the number is less than 52
+		if(integer< 52) {
+			System.out.println(integer);
+		}
+
+		
+	}
+
+}
