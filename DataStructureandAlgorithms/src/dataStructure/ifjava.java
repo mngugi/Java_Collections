f32656ad671e5cb0bf99ffa6e329ec69a6a971a6
mngugi <mngugi7@gmail.com>
2024-10-27 23:00:32 +0300

    Update ifjava.java

--- DataStructureandAlgorithms/src/dataStructure/ifjava.java
+++ DataStructureandAlgorithms/src/dataStructure/ifjava.java
@@ -2,13 +2,13 @@ package dataStructure;
 
 public class ifjava {
 	public static void main(String[] args) {
-		int integer = 65;
+		int integer = -5;
 		// check if the number is less than 0
 		if(integer < 0) {
 			System.out.println("The number is negative.");
 		}
 
-		    System.out.print(integer);
+		    System.out.print("Ststement outside the block.");
 	}
 
 }
