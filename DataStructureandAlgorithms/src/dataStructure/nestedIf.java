018867a828e832de081d20d0d0af55a4fd3f3daa
mngugi <mngugi7@gmail.com>
2024-10-28 13:00:53 +0300

    Update nestedIf.java

--- DataStructureandAlgorithms/src/dataStructure/nestedIf.java
+++ DataStructureandAlgorithms/src/dataStructure/nestedIf.java
@@ -2,7 +2,16 @@ package dataStructure;
 
 public class nestedIf {
 	public static void main(String[] args) {
-		Double 
+		Double num1 = -2.4, num2 = 6.3, num3 = 8.5, largest_number ;
+		
+		// check and compare the largest number
+		
+		if( num1 >= num2) {
+			largest_number = num1;
+			System.out.println("Larger number is" + largest_number );
+			
+		}
+			
 	}
 
 }
