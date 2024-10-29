7266b6714f9a5e6f9ce91725c9b1ba43e6980708
mngugi <mngugi7@gmail.com>
2024-10-29 12:25:31 +0300

    Update ternaryOperator.java

--- DataStructureandAlgorithms/src/dataStructure/ternaryOperator.java
+++ DataStructureandAlgorithms/src/dataStructure/ternaryOperator.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class ternaryOperator {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Marks: ");
+		double makrs = input.nextDouble();
 	}
 
 }
