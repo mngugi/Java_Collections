b15a51de416f3e262cf0c8b904ad5ff0635d06ca
mngugi <mngugi7@gmail.com>
2024-10-29 12:29:05 +0300

    Update ternaryOperator.java

--- DataStructureandAlgorithms/src/dataStructure/ternaryOperator.java
+++ DataStructureandAlgorithms/src/dataStructure/ternaryOperator.java
@@ -5,7 +5,11 @@ public class ternaryOperator {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Marks: ");
-		double makrs = input.nextDouble();
+		double marks = input.nextDouble();
+		
+		String results = (marks >= 40)? "pass" : "fail";
+		System.out.println("Exam" + results);
+		input.close();
 	}
 
 }
