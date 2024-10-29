b6433c053c3f785d9e0d504ac2095bea6cccbdec
mngugi <mngugi7@gmail.com>
2024-10-29 20:51:51 +0300

    Update ternary.java

--- DataStructureandAlgorithms/src/dataStructure/ternary.java
+++ DataStructureandAlgorithms/src/dataStructure/ternary.java
@@ -3,7 +3,7 @@ package dataStructure;
 public class ternary {
 	public static void main(String[] args) {
 		// variable
-		int j = 95;
+		int j = -95;
 		String result = (j > 0)? "Positive number" : "Negative number";
 		System.out.print(result);
 ;
