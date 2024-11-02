70cf7fd99d4bd903f656ba29b1f06250ab01c81c
M.Ngugi <mngugi7@gmail.com>
2024-11-02 18:02:50 +0300

    Update oneParam.java

--- Structures/src/structures/oneParam.java
+++ Structures/src/structures/oneParam.java
@@ -1,8 +1,20 @@
 package structures;
 
 public class oneParam {
-	public void Counter() {
-		this(0);
-	}
+    private int count;
 
+    // Constructor with one parameter
+    public oneParam(int count) {
+        this.count = count;
+    }
+
+    // Default constructor that calls the parameterized constructor
+    public oneParam() {
+        this(0); // Calls the constructor with count initialized to 0
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
+
