ec3376ec3d67577cecacbdaa90bccd5442d55d4c
M.Ngugi <mngugi7@gmail.com>
2024-11-02 14:43:50 +0300

    Update CounterDemo.java

--- Structures/src/structures/CounterDemo.java
+++ Structures/src/structures/CounterDemo.java
@@ -2,6 +2,17 @@ package structures;
 
 public class CounterDemo {
 	public static void main(String[] args) {
+		Counter c;
+		c = new Counter();
+		c.increment();
+		c.increment(4);
+		int temp = c.getCount();
+		c.reset();
+		Counter j = new Counter(5);
+		j.increment();
+		Counter i = j;
+		temp = i.getCount();
+		i.increment(2);
 		
 	}
 
