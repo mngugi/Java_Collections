eb33e0a7963796fb1446075fe942e93a1586b0d8
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-19 00:55:09 +0300

    lcab_labs/lcab1_20260118.java

---
+++ lcab_labs/lcab1_20260118.java
@@ -0,0 +1,19 @@
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        // Array example
+        int[] highScores = new int[10];
+        
+               // Add an array of 5 doubles called prices.
+        double[] prices = new double[5];
+
+        // Add an array of 5 Strings called names.
+         String[] names = new String[5];
+
+        System.out.println(
+                "Array highScores declared with size " + highScores.length);
+        // Print out the length of the new arrays
+    }
+}
+
---
+++ lcab_labs/lcab2_20260118.java
@@ -0,0 +1,9 @@
+public class Test2
+{
+    public static void main(String[] args)
+    {
+        int[] highScores = {99, 98, 98, 88, 68, 69};
+        System.out.println(highScores.length);
+    }
+}
+
