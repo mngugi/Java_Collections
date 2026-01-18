b55557f8d671d45a76e700323eb5bbee4ed4b309
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-19 00:55:37 +0300

    Create array-set_20260118.java

---
+++ lcab_labs/array-set_20260118.java
@@ -0,0 +1,15 @@
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        // declare, create, initialize arrays
+        int[] highScores = {99, 98, 98, 88, 68, 85};
+        String[] names = {"Jamal", "Emily", "Destiny", "Mateo", "Sofia", "Nous"};
+
+        // Print corresponding names and scores
+        System.out.println(names[0] + " has a score of " + highScores[0]);
+        System.out.println(names[1] + " has a score of " + highScores[1]);
+        System.out.println(names[3] + " has a score of " + highScores[3]);
+    }
+}
+
