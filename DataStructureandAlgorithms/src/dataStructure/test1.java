3c1ea29afc8da0e206a0c454e7726127b9efa5fa
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-20 17:21:51 +0300

    Create test1.java

---
+++ DataStructureandAlgorithms/src/dataStructure/test1.java
@@ -0,0 +1,20 @@
+public class test1 {
+
+    public static void main(String[] args) {
+
+        // scores arrays
+
+        int[] eplPoints = {69, 65, 78, 54, 89};
+
+        // declare a string
+        String[] teams = {"United", "leeds", "Chelsea", "WestHam", "Fulham"};
+
+        System.out.println(teams[1] + " has a total of " + eplPoints[1]);
+
+
+    }
+
+
+
+
+}
